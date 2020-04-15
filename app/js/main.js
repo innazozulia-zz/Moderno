a120d205f564681774af53fa979a9374b7521d91
innazozulia <innazozulia@outlook.com>
2020-04-15 17:24:36 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
+  $(".rate-star").rateYo({
+    rating: 5,
+    readOnly: true,
+    starWidth: "12px",
+  });
+
+  $(".product-slider__inner").slick({
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+  });
+
   var mixer = mixitup(".products__inner-box");
 });
