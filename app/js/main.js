d9177fe29c9033a2d801b0d63a9342850f8711a0
innazozulia <innazozulia@outlook.com>
2020-04-28 15:09:05 +0300

    added new-items and new-items list page, fixed old bugs

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,16 @@ $(function () {
     dots: true,
     infinite: true,
   });
-
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th-large").removeClass("active");
+  });
+  $(".icon-th-large").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
   // var range = document.getElementById("range");
 
   // noUiSlider.create(range, {
